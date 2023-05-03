09c937926f23a24bf45ba64ecccb701e64b19dea
dorLoki <luke.heydel@outlook.de>
2023-05-03 16:35:45 +0200

    copy, equals, hashcode

--- semester.2.praktika/Prog2/p5/Point.java
+++ semester.2.praktika/Prog2/p5/Point.java
@@ -1,5 +1,7 @@
 package p5;
 
+import java.util.Objects;
+
 public class Point {
 	private int x;
 	private int y;
@@ -21,5 +23,32 @@ public class Point {
 		this.x += dx;
 		this.y += dy;
 		return this;
+	}
+
+	/**
+	 * Clones a point object and return a new point and assures that this new point
+	 * is equal to this point.
+	 * 
+	 * @return a clone of the point object.
+	 */
+	public Point copy() {
+		return new Point(this.x, this.y);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(x, y);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Point other = (Point) obj;
+		return x == other.x && y == other.y;
 	}
 }
