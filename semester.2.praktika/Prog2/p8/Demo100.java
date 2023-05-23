ebd11d28368ebf245b85f4810a77aaf25cdd976e
dorLoki <luke.heydel@outlook.de>
2023-05-23 11:20:28 +0200

    p8 (1/2)

---
+++ semester.2.praktika/Prog2/p8/Demo100.java
@@ -0,0 +1,24 @@
+package p8;
+
+
+public class Demo100 implements Runnable {
+	Thread thread;
+
+	public Demo100() {
+		thread = new Thread(this);
+		thread.start();
+	}
+
+	public static void main(String[] args) {
+		for (int i = 0; i < 100000; i++) {
+			new Demo100();
+		}
+	}
+
+	@Override
+	public void run() {
+		for (int i = 0; i < 1000; i++) {
+			System.out.println(thread.getId());
+		}
+	}
+}
